c969fe0599f3f006d6e84fe794d793a2d9da0f20
Travis Cline <travis.cline@gmail.com>
2015-07-15 11:11:17 -0700

    Refactor QueryIter api

--- query_iter.go
+++ query_iter.go
@@ -1,9 +1,34 @@
 package parse
 
-// NewQueryIter returns an iterator to iterate over a query on a Parse Class ordered by
+// NewQueryIter returns an iterator to iterate over batches of Parse Objects ordered by
 // createdAt. It automatically manages Skip values to process the entire set of objects.
-func (c *Client) NewQueryIter(className string, whereClause string) (*QueryIter, error) {
-	return &QueryIter{client: c, where: whereClause, className: className}, nil
+//
+// The Parse Class to query is inferred from the type of destination.
+//
+// destination should be a slice of the class you would like to populate.
+//
+// After every call to Next() destination will have a new batch of objects.
+func (c *Client) NewQueryIter(whereClause string, destination interface{}) (*QueryIter, error) {
+	className, err := objectTypeNameFromSlice(destination)
+	if err != nil {
+		return nil, err
+	}
+	return c.NewQueryClassIter(className, whereClause, destination)
+}
+
+// NewQueryClassIter returns an iterator to iterate over batches of Parse Objects ordered by
+// createdAt. It automatically manages Skip values to process the entire set of objects.
+//
+// destination should be a slice of the class you would like to populate.
+//
+// After every call to Next() destination will have a new batch of objects.
+func (c *Client) NewQueryClassIter(className string, whereClause string, destination interface{}) (*QueryIter, error) {
+	return &QueryIter{
+		client:      c,
+		destination: destination,
+		where:       whereClause,
+		className:   className,
+	}, nil
 }
 
 // QueryIter allows you to iterate over all objects in a Parse Class.
@@ -12,46 +37,42 @@ type QueryIter struct {
 	className string
 	where     string
 
-	currentBatch []interface{}
-	index        int
-	lastErr      error
-	processed    uint
+	destination interface{}
+	lastErr     error
+	processed   int
 }
 
-func (s *QueryIter) fetchBatch() ([]interface{}, error) {
+func (i *QueryIter) fetchBatch() error {
 	where := QueryOptions{
-		Where: s.where,
+		Where: i.where,
 		Limit: 1000,
 		Order: "createdAt",
-		Skip:  int(s.processed),
+		Skip:  int(i.processed),
 	}
-	var currentBatch []interface{}
-	err := s.client.QueryClass(s.className, &where, &currentBatch)
-	return currentBatch, err
+	return i.client.QueryClass(i.className, &where, &i.destination)
 }
 
-// Next returns the next object. It will return nil when the iterator is exhausted or an error has occurred.
-// TODO sheki pass in interface in Next and serialize to it.
-func (s *QueryIter) Next() interface{} {
-	if s.index == 0 || s.index >= len(s.currentBatch) {
-		s.currentBatch, s.lastErr = s.fetchBatch()
-		if s.lastErr != nil {
-			return nil
-		}
-		s.index = 0
+// Next populates the provided slice with the next batch of objects or it will return false.
+func (i *QueryIter) Next() bool {
+	if i.lastErr = i.fetchBatch(); i.lastErr != nil {
+		return false
 	}
-	if len(s.currentBatch) == 0 {
-		// end of iteration
-		return nil
+	n := len(asInterfaceSlice(i.destination))
+	if n == 0 {
+		return false
 	}
-
-	current := s.currentBatch[s.index]
-	s.index++
-	s.processed++
-	return current
+	i.processed += n
+	return true
 }
 
 // Err returns nil if no errors happened during iteration, or the actual error otherwise.
-func (s *QueryIter) Err() error {
-	return s.lastErr
+func (i *QueryIter) Err() error {
+	return i.lastErr
+}
+
+func asInterfaceSlice(v interface{}) []interface{} {
+	if v == nil {
+		return nil
+	}
+	return v.([]interface{})
 }
